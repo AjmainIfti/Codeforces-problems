233bfad8984ef884854741fdccee7de30f3d9632
Ajmain Rahman Ifti <ajmainifti.05@gmail.com>
2025-02-21 23:53:31 +0600

    Sum

---
+++ 1742A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t, cases;
+    cin>>t;
+    for(cases=1; cases<=t; cases++){
+        int a, b, c;
+        cin>>a>>b>>c;
+
+        if(a+b==c || b+c==a || c+a==b)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+
+}
+}
