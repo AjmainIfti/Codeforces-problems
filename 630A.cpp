80031fad1f116862feb824b80ff7f0edd147675e
Ajmain Rahman Ifti <ajmainifti.05@gmail.com>
2025-04-14 00:28:09 +0600

    Again Twenty Five!

---
+++ 630A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n;
+    cin>>n;
+    long long result = pow(5, n);
+    int Final = 25;
+    cout<<Final;
+}
